f2272e568b00ee1afa164e6791557bbc8dc343bf
matheuscrok <matheusprogfut@outlook.com>
2021-09-03 23:25:56 -0300

    finalizado

--- App.js
+++ App.js
@@ -6,19 +6,18 @@ export default function App() {
   // Hooks - useState
   const [produto, setProduto] = useState('')
   const [produtos, setProdutos] = useState([])
-  const [count,setCount ]=useState(0);
   //SPREAD JS
   function handleAdicionarProduto() {
-    setCount(count+1);
-    setProdutos(vetorVelho => [...vetorVelho, {id:count,name:produto}])
+    setProdutos(vetorVelho => [...vetorVelho, produto])
     setProduto('')
 
   }
+
+
   function handleRemoverProduto(prodRemover) {
     setProdutos(vetorVelho => vetorVelho.filter(prod => prod !== prodRemover))
   }
 
-  console.warn(produtos);
 
 
   return (
@@ -28,51 +27,30 @@ export default function App() {
 
       <Button title="Adicionar" onPress={handleAdicionarProduto} onClick></Button>
       
-      {/* <Produto data={produtos[0]}/> */}
-      <View style={styles.container}>
-           
            <FlatList
-               data={produtos} keyExtractor={(item)=>produtos.indexOf(id).toString}
-               renderItem={( {item} ) => <Produto data={item}/>}
+               data={produtos} keyExtractor={(item) => item}
+               renderItem={({item} ) => <Produto data={item}/>}
                />
 
-       </View>
-
-      {/* <View> */}
-
-        {/* {
-
-          produtos.map(pro => (
-            <View key={pro} style={{ flexDirection: 'row' }}>
-              <Text>{pro}</Text>
-
-            </View>
-          )
-          )
-        } */}
-
-      {/* </View> */}
 
     </View>
   );
 }
 
-
-export function Produto({ data }) {
-  console.warn(data);
+function Produto({data}) {
   
     return (
-        <View key={data} style={styles.produto}>
-            <Text>Nome: {data.name}</Text>
+        <View style={styles.produto}>
+            <Text>Nome: {data}</Text>
             <Button title="x" onPress={() => handleRemoverProduto(data)}></Button>
         </View>
     );
 }
 
 
+
 const styles = StyleSheet.create({
   container:{
-      flex:1,
       marginTop:15
      
   },
